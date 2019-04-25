ffb15e79e43801e385da65df39d3d910e3fd6460
Kyle Walters <kyle.p.walters@gmail.com>
2019-04-25 15:17:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   return `It's ${holiday} days until ${days}!`
 }
